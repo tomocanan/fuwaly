589d58ba1e8aa45093b7f4ea6a9e2c83d652350a
tomocanan <happyflower1914@gmail.com>
2020-10-12 17:03:40 +0900

    ActiveHashの id変更

--- app/models/feeling.rb
+++ app/models/feeling.rb
@@ -1,10 +1,10 @@
 class Feeling < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--' },
-    { id: 2, name: '悪い' },
-    { id: 3, name: '少し悪い' },
-    { id: 4, name: '普通' },
-    { id: 5, name: 'まあま良い' },
-    { id: 6, name: '良い' },
+    { id: 0, name: '--' },
+    { id: 1, name: '悪い' },
+    { id: 2, name: '少し悪い' },
+    { id: 3, name: '普通' },
+    { id: 4, name: 'まあま良い' },
+    { id: 5, name: '良い' },
   ]
  end
--- app/models/happiness.rb
+++ app/models/happiness.rb
@@ -1,10 +1,10 @@
 class Happiness < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--' },
-    { id: 2, name: '低い' },
-    { id: 3, name: '少し低い' },
-    { id: 4, name: '普通' },
-    { id: 5, name: '少し高い' },
-    { id: 6, name: '高い' },
+    { id: 0, name: '--' },
+    { id: 1, name: '低い' },
+    { id: 2, name: '少し低い' },
+    { id: 3, name: '普通' },
+    { id: 4, name: '少し高い' },
+    { id: 5, name: '高い' },
   ]
  end
