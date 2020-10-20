f91f21192a725b362c4c49ed0b0e6f1bc3eda60d
tomocanan <happyflower1914@gmail.com>
2020-10-20 14:53:58 +0900

    Revert "マイグレーションファイル変更"
    This reverts commit 2d449afc0a1eb61d438f7f5cc0e5f70389b05415.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_06_054609) do
+ActiveRecord::Schema.define(version: 2020_10_15_033018) do
+
+  create_table "meetings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.datetime "start_time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "statuses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.date "date", null: false
